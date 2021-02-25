4f4c40ef771c97440de1673c853d26a1156b5094
wulabwn <achmadwulabwn.com>
2021-02-25 20:45:18 +0700

    angular_elearning

--- my-app/src/app/course/course.component.ts
+++ my-app/src/app/course/course.component.ts
@@ -20,4 +20,12 @@ export class CourseComponent implements OnInit {
     
     ]
 
+    buku = [
+      { 'id':2 ,'name' :'Biologi', 'description': 'Lorem ipsum dolor sit amet,Ut enim ad minim veniam','image':'../../assets/buku_debian_telkom.jpg','link_pdf':'buku_debian.pdf'},
+      { 'id':2 ,'name' :'kimia', 'description': 'Lorem ipsum dolor sit amet,Ut enim ad minim veniam','image':'../../assets/buku_debian_telkom.jpg','link_pdf':'buku_debian.pdf'},
+      { 'id':2 ,'name' :'Matematika', 'description': 'Lorem ipsum dolor sit amet,Ut enim ad minim veniam','image':'../../assets/buku_debian_telkom.jpg','link_pdf':'buku_debian.pdf'},
+      { 'id':2 ,'name' :'Fisika', 'description': 'Lorem ipsum dolor sit amet,Ut enim ad minim veniam','image':'../../assets/buku_debian_telkom.jpg','link_pdf':'buku_debian.pdf'}
+     
+     ]
+
 }
